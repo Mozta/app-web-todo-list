165e72a14f0248cc6b9fef1aa51385ced778cc82
Rafael <perezaguirrerafael@gmail.com>
2022-03-16 18:54:33 -0600

    for tasks

--- app.py
+++ app.py
@@ -5,7 +5,30 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    tasks = [
+        {
+            'id': 1,
+            'name': 'Tomar la clase',
+            'check': False
+        },
+        {
+            'id': 2,
+            'name': 'Hacer la tarea',
+            'check': False
+        },
+        {
+            'id': 3,
+            'name': 'Crear mi aplicación web de tareas',
+            'check': False
+        },
+        {
+            'id': 4,
+            'name': 'Crear mi aplicación web de tareas',
+            'check': False
+        },
+    ]
+
+    return render_template('index.html', tasks=tasks)
 
 if __name__ == '__main__':
     app.run(debug=True)
